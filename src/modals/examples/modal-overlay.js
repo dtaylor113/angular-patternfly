53858531ef0c4ddd0ce8f23a88d2eb25731d8a2e
nthoen <nicolemthoen@gmail.com>
2018-03-15 10:36:43 -0400

    add action tracking and some debugging for form validation

--- src/modals/examples/modal-overlay.js
+++ src/modals/examples/modal-overlay.js
@@ -18,7 +18,6 @@
  * @param {boolean} isOpen Flag indicating that the modal should be opened
  * @param {function} onClose Function to call when the modal is closed
  * @param {string} modalId Id of the modal
- * @param {string} modalBodyPath The path to the html template to populate the modal body
  * @param {string} modalTitle The title of the modal displayed in the header
  * @param {string=} titleId Id of the title. "modalTitle" by default
  * @param {boolean=} hideCloseIcon Flag indicating that the modal should hide the 'x' close icon.
@@ -41,32 +40,36 @@
     <pf-modal-overlay is-open="isOpen"
           on-close="onClose()"
           modal-id="modalId"
-          modal-body-path="modalBodyPath"
           modal-title="modalTitle"
-          action-buttons="actionButtons">
+          action-buttons="actionButtons"
+          is-invalid="demoForm.$invalid || demoForm.$pristine">
+       {{"is-invalid = " + demoForm.$invalid || demoForm.$pristine}}
        <form name="demoForm" class="form-horizontal">
          <div class="form-group">
-           <label class="col-sm-3 control-label" for="textInput">Field One</label>
+           <label class="col-sm-3 control-label required-pf" for="textInput">Field One</label>
            <div class="col-sm-9">
-              <input type="text" id="textInput" class="form-control"/>
+              <input type="text" id="textInput" class="form-control" ng-model="inputs.first" ng-required="true"/>
            </div>
          </div>
          <div class="form-group">
            <label class="col-sm-3 control-label" for="textInput2">Field Two</label>
-           <div class="col-sm-9"><input type="text" id="textInput2" class="form-control"/></div>
+           <div class="col-sm-9">
+              <input type="text" id="textInput2" class="form-control" ng-model="inputs.second"/>
+           </div>
          </div>
          <div class="form-group">
            <label class="col-sm-3 control-label" for="textInput3">Field Three</label>
-           <div class="col-sm-9"><input type="text" id="textInput3" class="form-control"/></div>
+           <div class="col-sm-9">
+              <input type="text" id="textInput3" class="form-control" ng-model="inputs.third"/>
+           </div>
          </div>
        </form>
- </pf-modal-overlay>
+    </pf-modal-overlay>
 
    <button ng-click="open2()" class="btn btn-default">Launch Second Modal Overlay</button>
    <pf-modal-overlay is-open="isOpen2"
          on-close="onClose2()"
          modal-id="modalId2"
-         modal-body-path="modalBodyPath2"
          modal-title="modalTitle2"
          hide-close-icon="hideCloseIcon2"
          backdrop-close="backdropClose"
@@ -75,14 +78,26 @@
        <div class="row">
           <div class="col-md-12">Donec consequat dignissim neque, sed suscipit quam egestas in. Fusce bibendum laoreet lectus commodo interdum. Vestibulum odio ipsum, tristique et ante vel, iaculis placerat nulla. Suspendisse iaculis urna feugiat lorem semper, ut iaculis risus tempus.</div>
        </div>
- </pf-modal-overlay>
+    </pf-modal-overlay>
+   <div>
+      <label class="actions-label">Actions: </label>
+   </div>
+   <div>
+      <textarea rows="3">{{actionsText}}</textarea>
+   </div>
  </div>
  </file>
 
  <file name="script.js">
- angular.module('patternfly.modals').controller('DemoModalOverlayCtrl', function( $scope, $log ) {
+ angular.module('patternfly.modals').controller('DemoModalOverlayCtrl', function( $scope ) {
 
       // first example
+      $scope.inputs = {
+        first: "",
+        second: "",
+        third: ""
+      };
+      $scope.actionsText = "";
       $scope.open = function () {
           $scope.isOpen = true;
        };
@@ -92,17 +107,23 @@
 
       $scope.modalId = "demoModal1";
       $scope.modalTitle = "First Demo Title";
-      $scope.modalBodyPath = 'pf-modal-body.html';
       $scope.actionButtons = [
           {
             label: "Cancel",
-            isCancel: true
+            isCancel: true,
+            actionFn: function() {
+              $scope.actionsText = "Cancel clicked\n" + $scope.actionsText;
+            }
           },
           {
             label: "Save",
             class: "btn-primary custom-class",
             actionFn: function() {
-              $log.info("Save clicked");
+                $scope.actionsText = "Save clicked" +
+                    "\nFirst Field" + $scope.inputs.first +
+                    "\nSecond Field: " + $scope.inputs.second +
+                    "\nThird Field" + $scope.inputs.third +
+                    "\n" + $scope.actionsText;
             }
           }];
 
@@ -117,17 +138,22 @@
       $scope.modalId2 = "demoModal2";
       $scope.modalTitle2 = "Second Demo Title";
       $scope.titleId2 = "demoTitle2";
-      $scope.modalBodyPath2 = 'pf-modal-body2.html';
       $scope.hideCloseIcon2 = true;
       $scope.backdropClose = true;
       $scope.actionButtons2 = [
           {
             label: "Cancel",
-            isCancel: true
+            isCancel: true,
+            actionFn: function() {
+              $scope.actionsText = "Second Cancel clicked\n" + $scope.actionsText;
+            }
           },
           {
             label: "OK",
-            class: "btn-primary"
+            class: "btn-primary",
+            actionFn: function() {
+              $scope.actionsText = "OK clicked\n" + $scope.actionsText;
+            }
           }];
  });
 
