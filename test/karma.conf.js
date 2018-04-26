f277ea712ad16d6782df7b83d4130f941d2782af
David Taylor <dtaylor@redhat.com>
2018-04-26 10:30:28 -0400

    browserNoActivityTimeout 30000

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -97,6 +97,7 @@ module.exports = function (config) {
     // If browser does not capture in given timeout [ms], kill it
     // CLI --capture-timeout 5000
     captureTimeout: 20000,
+    browserNoActivityTimeout: 30000,
 
     // Auto run tests on start (when browsers are captured) and exit
     // CLI --single-run --no-single-run
